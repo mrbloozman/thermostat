f3521664fbc2f4e497a513486c598c70668aed6b
Mark Schwartz <mrbloozman@gmail.com>
2018-02-02 06:53:40 +0000

    menu button mapping

--- python/display_driver.py
+++ python/display_driver.py
@@ -18,34 +18,62 @@ class screen(enum.Enum):
 
 status = {
 	'screen': -1,
-	'screenNext': 0
+	'screenNext': screen.main,
+	'touchPoint':{},
+	'message': 'startup'
 }
 
 def handleInterrupt(channel):
+	print 'handleInterrupt(' + channel + ')'
 	global status
-	while not GPIO.input(RA8875_INT):
-		if tft.touched():
-			point = tft.touchRead()
-			print point
-			if status['screen'] == screen.main:
-				status['screenNext'] = screen.menu
-			elif status['screen'] == screen.menu:
-				status['screenNext'] = screen.main
+	while tft.touched():
+		status['touchPoint'] = tft.touchRead()
+		print status['touchPoint']
+		if status['screen'] == screen.main:
+			status['screenNext'] = screen.menu
+		elif status['screen'] == screen.menu:
+			if status['touchPoint']['x'] < int(1024/3) and status['touchPoint']['y'] < 1024/2:
+				status['message'] = 'touched button 1'
+			elif status['touchPoint']['x'] < int(2*1024/3) and status['touchPoint']['y'] < 1024/2:
+				status['message'] = 'touched button 2'
+			elif status['touchPoint']['y'] < 1024/2:
+				status['message'] = 'touched button 3'
+			elif status['touchPoint']['x'] < int(1024/3) and status['touchPoint']['y'] >= 1024/2:
+				status['message'] = 'touched button 4'
+			elif status['touchPoint']['x'] < int(2*1024/3) and status['touchPoint']['y'] >= 1024/2:
+				status['message'] = 'touched button 5'
+			elif status['touchPoint']['y'] >= 1024/2:
+				status['message'] = 'touched button 6'
+			else:
+				status['message'] = 'no button touched'
+			status['screenNext'] = screen.main
+		
 
 
 def renderMainScreen(data):
+	print 'renderMainScreen(' + str(data) + ')'
 	global status
 	status['screen'] = screen.main
+	tft.fillScreen(RA8875_BLUE)
 	# may need to monitor wait signal in textMode
 	tft.textMode()
-	tft.textSetCursor(0,0)
+	tft.textEnlarge(3)
 	tft.textColor(RA8875_WHITE,RA8875_BLUE)
-	tft.textWrite('Test',0)
+	tft.textSetCursor(0,0)
+	tft.textWrite(status['message'],0)
+	tft.graphicsMode()
 
 def renderMenuScreen(data):
+	print 'renderMenuScreen(' + str(data) + ')'
 	global status
 	status['screen'] = screen.menu
 	tft.fillScreen(RA8875_RED)
+	tft.drawRect(0, 0, int(800/3), 240, RA8875_YELLOW)
+	tft.drawRect(int(800/3), 0, int(800/3), 240, RA8875_YELLOW)
+	tft.drawRect(int(2*800/3), 0, int(800/3), 240, RA8875_YELLOW)
+	tft.drawRect(0, 240, int(800/3), 239, RA8875_YELLOW)
+	tft.drawRect(int(800/3), 240, int(800/3), 239, RA8875_YELLOW)
+	tft.drawRect(int(2*800/3), 240, int(800/3), 239, RA8875_YELLOW)
 	
 
 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET)
@@ -64,10 +92,15 @@ tft.fillScreen(RA8875_BLUE)
 tft.touchEnable(True)
 
 GPIO.setup(RA8875_INT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(RA8875_INT, GPIO.FALLING, callback=handleInterrupt, bouncetime=300)
+# GPIO.add_event_detect(RA8875_INT, GPIO.FALLING, callback=handleInterrupt, bouncetime=300)
 
 while True:
 	try:
+		print 'INT PIN: ' + str(GPIO.input(RA8875_INT))
+		print 'INTC1: ' + hex(tft.readReg(RA8875_INTC1))
+		print 'INTC2: ' + hex(tft.readReg(RA8875_INTC2))
+		if GPIO.input(RA8875_INT)==0:
+			handleInterrupt(RA8875_INT)
 		if status['screen'] != status['screenNext']:
 			if status['screenNext'] == screen.main:
 				data = {}
@@ -75,6 +108,8 @@ while True:
 			elif status['screenNext'] == screen.menu:
 				data = {}
 				renderMenuScreen(data)
+
+		sleep(1)
 
 	except KeyboardInterrupt:
 		GPIO.cleanup()
