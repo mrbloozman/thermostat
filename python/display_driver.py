7ae800a2dc52e974f92c6c5a51d09007cf170d71
Mark Schwartz <mrbloozman@gmail.com>
2018-02-03 11:18:09 -0800

    Input, Button, and Spinbox classes

--- python/display_driver.py
+++ python/display_driver.py
@@ -12,12 +12,42 @@ RA8875_INT = 'XIO-P1'
 RA8875_CS = 'XIO-P2'
 RA8875_RESET = 'XIO-P3'
 
-class screen(enum.Enum):
+class t_screen(enum.Enum):
 	main = 0
 	menu = 1
 
-class button:
-	def __init__(self,x,y,w,h,fg_color,bg_color,label,callback):
+class t_datatype(enum.Enum):
+	text = 0
+	number = 1
+	boolean = 2
+	submit = 3
+
+# do I need multi-line labels?
+class Label:
+	def __init__(self,text,size):
+		self._text = str(text)
+		self._size = int(size) # text enlargement size 0-3
+		
+	def text(self,t=None):
+		if t:
+			self._text = str(t)
+		return self._text
+
+	def size(self,s=None):
+		if s:
+			self._size = int(s)
+		return self._size
+
+	# assuming 1x cursor size is 8x16 pixels
+	def width(self):
+		return len(text)*8*(self._size+1)
+
+	def height(self):
+		return 16*(self._size+1) # assuming single line
+
+
+class Input:
+	def __init__(self,x,y,w,h,fg_color,bg_color,label,datatype):
 		self.x = int(x)
 		self.y = int(y)
 		self.w = int(w)
@@ -26,6 +56,70 @@ class button:
 		self.bg_color = bg_color
 		self.label = label
 		self.enabled = True
+		self._datatype = datatype
+
+		if self._datatype == t_datatype.text:
+			self.value = ''
+		elif self._datatype == t_datatype.number:
+			self.value = 0
+		else:
+			self.value = False
+
+# Spinbox has a tapped event (passed between both buttons)
+class Spinbox:
+	def __init__(self,x,y,fg_color,bg_color,label,mn,mx,callback):
+		self._callback = callback
+		self._min = int(mn)
+		self._max = int(mx)
+		btnw = (label.size()*16)
+		btnh = (label.size()*32)
+		w = label.width() + btnw
+		h = label.height() + btnh
+		Input.__init__(self,x,y,w,h,fg_color,bg_color,label,t_datatype.number)
+
+		btnx = self._x+self.label.width()
+		self._upBtn = Button(btnx,self._y+btnh,btnw,btnh,fg_color,bg_color,Label('up',0),self.increase)
+		self._dnBtn = Button(btnx,self._y,btnw,btnh,fg_color,bg_color,Label('dn',0),self.decrease)
+
+	def render(self):
+		if self.enabled:
+			bg_color = self.bg_color
+			self._upBtn.enabled = True
+			self._dnBtn.enabled = True
+		else:
+			bg_color = RA8875_BLACK
+			self._upBtn.enabled = False
+			self._dnBtn.enabled = False
+		tft.graphicsMode()
+		tft.fillRect(self.x,self.y,self.w,self.h,bg_color)
+		tft.drawRect(self.x,self.y,self.w,self.h,self.fg_color)
+		tft.textMode()
+		tft.textColor(self.fg_color,bg_color)
+		tft.textSetCursor(self.x,self.y+int(self.h/2))
+		tft.textEnlarge(self.label.size())
+		tft.textWrite(self.label.text(),0)
+		self._upBtn.render()
+		self._dnBtn.render()
+
+	def increase(self):
+		if self.value < self._max:
+			self.value = self.value + 1
+
+	def decrease(self):
+		if self.value > self._min:
+			self.value = self.value - 1
+
+	def tapped(self,touchPoint):
+		if self._upBtn.tapped(touchPoint):
+			return True
+		elif self._dnBtn.tapped(touchPoint):
+			return True
+		return False
+
+# Button has a tapped event
+class Button:
+	def __init__(self,x,y,w,h,fg_color,bg_color,label,callback):
+		Input.__init__(self,x,y,w,h,fg_color,bg_color,label,t_datatype.boolean)
 		self._callback = callback
 
 	def render(self):
@@ -39,8 +133,8 @@ class button:
 		tft.textMode()
 		tft.textColor(self.fg_color,bg_color)
 		tft.textSetCursor(self.x+10,self.y+int(self.h/2))
-		tft.textWrite(self.label,0)
-		self._active = True
+		tft.textEnlarge(self.label.size())
+		tft.textWrite(self.label.text(),0)
 
 	def tapped(self,touchPoint):
 		if not self.enabled:
@@ -78,7 +172,7 @@ class menu:
 				x = c*w
 				y = r*h
 				bd = self._button_data[(r*self._cols)+c]
-				self._buttons.append(button(x,y,w,h,self.fg_color,self.bg_color,bd['label'],bd['callback']))
+				self._buttons.append(Button(x,y,w,h,self.fg_color,self.bg_color,bd['label'],bd['callback']))
 
 	def buttons(self):
 		return self._buttons
@@ -98,7 +192,7 @@ class menu:
 		# rows=2
 		# cols=3
 		# labels=['System On','Button 2','Button 3','System Off','Button 5','Button 6']
-		tft.textEnlarge(1)
+		# tft.textEnlarge(1)
 
 		for b in self._buttons:
 			b.render()
@@ -108,7 +202,7 @@ class menu:
 
 status = {
 	'screen': -1,
-	'screenNext': screen.main,
+	'screenNext': t_screen.main,
 	'touchPoint':{},
 	'message': 'startup',
 	'enable': 'off' # on/off
@@ -119,18 +213,18 @@ def handleInterrupt(channel):
 	global status
 	while tft.touched():
 		status['touchPoint'] = tft.touchRead()
-		if status['screen'] == screen.main:
-			status['screenNext'] = screen.menu
-		elif status['screen'] == screen.menu:
+		if status['screen'] == t_screen.main:
+			status['screenNext'] = t_screen.menu
+		elif status['screen'] == t_screen.menu:
 			for b in menu_screen.buttons():
 				if b.tapped(status['touchPoint']):
 					status['message'] = b.label + ' tapped'
-			status['screenNext'] = screen.main
+			status['screenNext'] = t_screen.main
 
 def renderMainScreen(data):
 	print 'renderMainScreen(' + str(data) + ')'
 	global status
-	status['screen'] = screen.main
+	status['screen'] = t_screen.main
 	tft.fillScreen(RA8875_BLUE)
 	# may need to monitor wait signal in textMode
 	tft.textMode()
@@ -178,25 +272,25 @@ GPIO.setup(RA8875_INT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 menu_screen_buttons = [{
-	'label':'System On',
+	'label': Label('System On',1),
 	'callback':enable
 	},{
-	'label':'Button 2',
+	'label':Label('Button 2',1),
 	'callback':test
 	},{
-	'label':'Button 4',
+	'label':Label('Button 4',1),
 	'callback':test
 	},{
-	'label':'System Off',
+	'label':Label('System Off',1),
 	'callback':disable
 	},{
-	'label':'Button 5',
+	'label':Label('Button 5',1),
 	'callback':test
 	},{
-	'label':'Button 6',
+	'label':Label('Button 6',1),
 	'callback':test
 	}]
-menu_screen = menu(screen.menu,2,3,menu_screen_buttons,RA8875_YELLOW,RA8875_RED)
+menu_screen = menu(t_screen.menu,2,3,menu_screen_buttons,RA8875_YELLOW,RA8875_RED)
 
 while True:
 	try:
@@ -206,10 +300,10 @@ while True:
 		if GPIO.input(RA8875_INT)==0:
 			handleInterrupt(RA8875_INT)
 		if status['screen'] != status['screenNext']:
-			if status['screenNext'] == screen.main:
+			if status['screenNext'] == t_screen.main:
 				data = {}
 				renderMainScreen(data)
-			elif status['screenNext'] == screen.menu:
+			elif status['screenNext'] == t_screen.menu:
 				data = {}
 				# renderMenuScreen(data)
 				menu_screen.render()
